deee8836332c2b5f46829f141817766e1d0633e7
David <dzz0615@gmail.com>
2012-01-28 14:28:09 -0500

    added main c file

---
+++ server/main.c
@@ -0,0 +1,15 @@
+/*
+** a datagram sockets "server" demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+int main(void)
+{
+	exit(EXIT_SUCCESS);
+}
+
