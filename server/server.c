728fb5556b093a91fe031338aa403c6ce282a4b8
David <dzz0615@gmail.com>
2012-01-29 15:32:06 -0500

    reorganized?

--- server/server.c
+++ server/server.c
@@ -14,7 +14,7 @@
 #include <netdb.h>
 #include <stdint.h>
 
-#include "../client/network.h"
+#include "network.h"
 
 #define MYPORT "4950"	// the port users will be connecting to
 
@@ -108,7 +108,8 @@ int32_t serverReceive()
 	pair = buf;
 
 	//iterate through all the points, divide by 8 = 4 byte ints * 2 ints per Coor
-	for(int32_t i=0; i<(numbytes/8); i++)
+	int32_t i;
+	for(i=0; i<(numbytes/8); i++)
 	{
 		printf("point %d: x=%d y=%d\n", (i+1), 
 				(int32_t)(*(pair)), (int32_t)(*(pair+4)));
@@ -127,7 +128,6 @@ int32_t main(void)
 
 	while(1)
 	{
-		perror("SHIT");
 		//if((err = serverReceive())) {
 			//exit(err);
 		//}
@@ -135,80 +135,9 @@ int32_t main(void)
 		serverReceive();
 	}
 
-	perror("MEEEEEEOWWWWWWWW");
 	if((err = serverClose())) {
 		exit(err);
 	}
 	exit(err);
 }
 
-/*int sockfd;
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	int numbytes;
-	struct sockaddr_storage their_addr;
-	char buf[MAXBUFLEN];
-	socklen_t addr_len;
-	char s[INET6_ADDRSTRLEN];
-	Coor xy[4];
-
-	memset(&xy, 0, 4*sizeof(Coor));
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
-	hints.ai_socktype = SOCK_DGRAM;
-	hints.ai_flags = AI_PASSIVE; // use my IP
-
-	if ((rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
-	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("listener: socket");
-			continue;
-		}
-
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("listener: bind");
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL) {
-		fprintf(stderr, "listener: failed to bind socket\n");
-		return 2;
-	}
-
-	freeaddrinfo(servinfo);
-
-	printf("listener: waiting to recvfrom...\n");
-
-	addr_len = sizeof(their_addr);
-	if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
-		(struct sockaddr *)&their_addr, &addr_len)) == -1) {
-		perror("recvfrom");
-		exit(1);
-	}
-
-	printf("listener: got packet from %s\n",
-		inet_ntop(their_addr.ss_family,
-			get_in_addr((struct sockaddr *)&their_addr),
-			s, sizeof(s)));
-	printf("listener: packet is %d bytes long\n", numbytes);
-
-	xy[0].x = (int32_t)(*buf);
-	xy[0].y = (int32_t)(*(buf+4));
-
-	//buf[numbytes] = '\0';
-	
-	printf("listener: packet contains \"%d, %d\"\n", xy[0].x, xy[0].y);
-
-	close(sockfd);
-
-	return 0;*/
