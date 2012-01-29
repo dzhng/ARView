84574d8d19a0370856785cf32398ee89253d3ff0
Ivan Stalev <shevbg@gmail.com>
2012-01-28 19:44:09 -0500

    it works now

--- client/client.c
+++ client/client.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <stdint.h>
 
 // socket includes
 #include <sys/types.h>
@@ -15,31 +16,26 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-// openCV includes
-#include "cv.h"
-#include "highgui.h"
+#include "network.h"
 
 #define SERVERPORT "4950"	// the port users will be connecting to
 
-int main(int argc, char *argv[])
+#define NUM_COOR 5
+
+int32_t sockfd;
+struct addrinfo hints, *servinfo, *p;
+int32_t rv;
+int32_t numbytes;
+char* hostname;
+
+int32_t clientInit(char hostname[])
 {
-	int sockfd;
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	int numbytes;
-
-	//make sure there are exactly 3 arguments...
-	//the client, hostname, and message
-	if (argc != 3) {
-		fprintf(stderr,"usage: client hostname message\n");
-		exit(1);
-	}
-
+	printf("hostname: %s\n", hostname);
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_socktype = SOCK_DGRAM;  //UDP
 
-	if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(hostname, SERVERPORT, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
@@ -59,6 +55,98 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "talker: failed to bind socket\n");
 		return 2;
 	}
+	return 0;
+}
+
+int32_t clientClose()
+{
+	freeaddrinfo(servinfo);
+
+	printf("talker: sent %d bytes to hostname\n", numbytes);
+	close(sockfd);
+	return 0;
+}
+
+//params: array of coordinates (xy) and size of array (n)
+int32_t clientSendCoor(Coor *xy, int32_t n)
+{
+			     //socket, message, length
+	if ((numbytes = sendto(sockfd, xy, sizeof(Coor)*n, 0,
+			 p->ai_addr, p->ai_addrlen)) == -1) {
+		perror("talker: sendto");
+		return 1;
+	}
+	return 0;
+}
+
+int32_t main(int32_t argc, char *argv[])
+{
+	hostname = argv[1];
+	int32_t err=0;
+	Coor xy[NUM_COOR];
+	xy[0].x = 1;
+	xy[0].y = 8;
+	xy[1].x = 2;
+	xy[1].y = 2;
+	xy[2].x = 3;
+	xy[2].y = 3;
+	xy[3].x = 4;
+	xy[3].y = 4;
+	xy[4].x = 13;
+	xy[4].y = 11;
+
+	if((err = clientInit(hostname))) {
+		exit(err);
+	}
+
+	while(!clientSendCoor(xy, NUM_COOR))
+	{
+		sleep(2);
+	}
+
+	if((err = clientClose())) {
+		exit(err);
+	}
+
+	exit(err);
+}
+
+/*int sockfd;
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	int numbytes;
+
+	//make sure there are exactly 3 arguments...
+	//the client, hostname, and message
+	if (argc != 3) {
+		fprintf(stderr,"usage: client hostname message\n");
+		exit(1);
+	}
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;  //UDP
+
+	if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		exit(1);
+	}
+
+	// loop through all the results and make a socket
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("talker: socket");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "talker: failed to bind socket\n");
+		exit(2);
+	}
 
 	if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
 			 p->ai_addr, p->ai_addrlen)) == -1) {
@@ -69,6 +157,4 @@ int main(int argc, char *argv[])
 	freeaddrinfo(servinfo);
 
 	printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
-	close(sockfd);
-}
-
+	close(sockfd);*/
